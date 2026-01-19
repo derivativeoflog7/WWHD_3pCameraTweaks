70eb9a0c35b94c523d3f62807645181b315528a6
(x⋅ln(7))⁻¹ <100133857+derivativeoflog7@users.noreply.github.com>
2026-01-19 15:54:33 +0100

    Bump version

--- Aroma/src/common.h
+++ Aroma/src/common.h
@@ -3,7 +3,7 @@
 
 #define PLUGIN_NAME "WWHD 3rd person camera tweaks"
 #define PLUGIN_DESCRIPTION "3rd camera person tweaks for TLOZ: The Wind Waker HD"
-#define PLUGIN_VERSION "v2.0"
+#define PLUGIN_VERSION "v2.1"
 #define PLUGIN_AUTOR "derivativeoflog7"
 #define PLUGIN_LICENSE "GPLv3"
 #define PLUGIN_CONFIG_ID "WWHD_3pCameraTweaks"
